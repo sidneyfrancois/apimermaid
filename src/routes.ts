16fb99056c5ba9e9769146344fbfdb18ff989159
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 21:01:53 -0300

    Inserindo rota de listagem de orders

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateOrderController } from "./controllers/CreateOrderController";
+import { ListAllOrdersController } from "./controllers/ListAllOrdersController";
 
 const routes = Router();
 
+routes.get("/", new ListAllOrdersController().handle);
 routes.post("/orders", new CreateOrderController().handle);
 
 export { routes };
