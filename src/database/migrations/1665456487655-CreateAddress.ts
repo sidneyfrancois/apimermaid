b92dbbcb9db3df007053154df6aca900fe3d9fb4
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 23:54:10 -0300

    Criando migration de Address

---
+++ src/database/migrations/1665456487655-CreateAddress.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAddress1665456487655 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "addresses",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+          },
+          {
+            name: "estado",
+            type: "varchar",
+          },
+          {
+            name: "cidade",
+            type: "varchar",
+          },
+          {
+            name: "bairro",
+            type: "varchar",
+          },
+          {
+            name: "rua",
+            type: "varchar",
+          },
+          {
+            name: "numero",
+            type: "numeric",
+          },
+          {
+            name: "cep",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("addresses");
+  }
+}
