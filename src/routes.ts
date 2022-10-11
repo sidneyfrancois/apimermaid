f2c16a5ac3ea1579ff785db96fefbac6baadc880
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 23:44:18 -0300

    Definindo rota de criação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreateOrderController } from "./controllers/CreateOrderController";
+import { CreateUserController } from "./controllers/CreateUserController";
 import { ListAllOrdersController } from "./controllers/ListAllOrdersController";
 
 const routes = Router();
 
 routes.get("/", new ListAllOrdersController().handle);
 routes.post("/orders", new CreateOrderController().handle);
+routes.post("/users", new CreateUserController().handle);
 
 export { routes };
