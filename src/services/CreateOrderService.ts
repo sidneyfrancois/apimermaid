3059ee8625440561deb36ff96e3fd656ab6f797a
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-10 21:22:35 -0300

    Inserindo funcionalidade Twilio

--- src/services/CreateOrderService.ts
+++ src/services/CreateOrderService.ts
@@ -1,5 +1,7 @@
 import { Order } from "../entities/Order";
 import { AppDataSource } from "../database";
+import { Twilio } from "twilio";
+require("dotenv").config();
 
 interface IOrderRequest {
   productName: string;
@@ -25,6 +27,22 @@ class CreateOrderService {
     });
 
     await repository.save(order);
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+    const myNumber = process.env.MY_NUMBER;
+
+    const client = new Twilio(accountSid, authToken);
+
+    client.messages
+      .create({
+        from: twilioNumber,
+        to: myNumber,
+        body: `Olá, a sua compra foi confirmada!
+        valor total da compra: ${totalValue}`,
+      })
+      .then((message) => console.log(message.sid));
 
     return order;
   }
