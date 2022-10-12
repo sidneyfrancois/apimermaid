a857068977bd309d987b2a0c6394682f99cb6984
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-12 15:46:26 -0300

    Criando entidade Frete

---
+++ src/entities/Frete.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("frete")
+class Frete {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  forma_envio: string;
+
+  @Column()
+  preco_envio: number;
+
+  @Column()
+  observacoes: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
