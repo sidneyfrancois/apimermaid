32569928dc9642fe8b19a1a78b6ebaacfc585048
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-12 16:39:22 -0300

    Criando serviço de autenticação

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { AppDataSource } from "../database";
+import { User } from "../entities/User";
+
+interface IRequestUser {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IRequestUser): Promise<IResponse> {
+    const repository = AppDataSource.getRepository(User);
+
+    const user = await repository.findOneBy({ email: email });
+
+    if (!user) {
+      throw new Error("User or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User or password incorrect");
+    }
+
+    const token = sign({}, "318450870ed5fa6e63092b8933f28bb8", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserService };
