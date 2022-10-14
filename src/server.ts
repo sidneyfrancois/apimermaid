6e75c888a979f38cbaf7d01f32c87e96199ce396
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-14 11:16:12 -0300

    Adicionando cors para corrigir CORS policy origin

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./database";
 import { routes } from "./routes";
+import cors from "cors";
 require("dotenv").config();
+
+const allowedOrigins = ["http://127.0.0.1:5173"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 AppDataSource.initialize()
   .then(() => {
     const app = express();
 
+    app.use(cors(options));
     app.use(express.json());
     app.use(routes);
 
